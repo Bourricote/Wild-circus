a3b13aa78f84e1d3c0076e2e020fc995f3b52ea3
Anne Quiedeville <anne.quiedeville@orange.fr>
2020-01-29 13:50:08 +0100

    done basis for homepage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PerformanceRepository;
+use App\Repository\TourRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,12 +12,15 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home(PerformanceRepository $performanceRepository)
+    public function home(PerformanceRepository $performanceRepository, TourRepository $tourRepository)
     {
-        $performances = $performanceRepository->findAll();
+        $performances = $performanceRepository->findBy([], ['createdAt' => 'ASC'], 3, 0 );
+
+        $nextTour = $tourRepository->findBy([], ['date' => 'ASC'], 1, 0);
 
         return $this->render('home.html.twig', [
             'performances' => $performances,
+            'next_tour' => $nextTour,
         ]);
     }
 }
