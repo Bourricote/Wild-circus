4e4c3120ab065c260562a0f4b81a42815397fe33
Anne Quiedeville <anne.quiedeville@orange.fr>
2020-01-29 13:04:14 +0100

    made CRUD for all entities

---
+++ src/Form/BookingType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nbTickets')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('user')
+            ->add('tour')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
